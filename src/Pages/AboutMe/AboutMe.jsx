39edafe73fc388eb9d0fe7c1e1c7dff0fc6711c6
TurjoSiam <t.siam24@gmail.com>
2025-05-03 23:12:32 +0600

    [ADDED]-hover effect added in banner buttons

--- src/Pages/AboutMe/AboutMe.jsx
+++ src/Pages/AboutMe/AboutMe.jsx
@@ -1,7 +1,7 @@
-import { FaGraduationCap } from "react-icons/fa";
+import { FaCode, FaGraduationCap } from "react-icons/fa";
 import 'animate.css';
-import { VscVscode } from "react-icons/vsc";
 import { MdBuild, MdCastForEducation } from "react-icons/md";
+import { AiFillCode } from "react-icons/ai";
 
 const AboutMe = () => {
     return (
@@ -10,14 +10,26 @@ const AboutMe = () => {
             <div className="w-11/12 md:w-full lg:w-10/12 mx-auto space-y-3">
                 <div className="flex items-center py-4 gap-5 md:gap-0 rounded-lg px-5 md:px-8 bg-[#eff1ec] justify-between">
                     <div className="flex items-center gap-4">
-                        <VscVscode className="text-4xl" />
+                        <AiFillCode className="text-4xl" />
+                        <div>
+                            <h2 className="font-bold">Next Level Web Development</h2>
+                            <p>Programming Hero</p>
+                        </div>
+                    </div>
+                    <div>
+                        <h2>2025 - Running</h2>
+                    </div>
+                </div>
+                <div className="flex items-center py-4 gap-5 md:gap-0 rounded-lg px-5 md:px-8 bg-[#eff1ec] justify-between">
+                    <div className="flex items-center gap-4">
+                        <FaCode className="text-4xl" />
                         <div>
                             <h2 className="font-bold">Complete Web Development</h2>
                             <p>Programming Hero</p>
                         </div>
                     </div>
                     <div>
-                        <h2>2024 - Running</h2>
+                        <h2>2024 - 2025</h2>
                     </div>
                 </div>
                 <div className="flex items-center justify-between py-4 rounded-lg px-8 bg-[#eff1ec]">
@@ -46,7 +58,7 @@ const AboutMe = () => {
                 </div>
                 <div className="flex items-center justify-between py-4 rounded-lg px-8 bg-[#eff1ec]">
                     <div className="flex items-center gap-4">
-                    <FaGraduationCap className="text-4xl" />
+                        <FaGraduationCap className="text-4xl" />
                         <div>
                             <h2 className="font-bold">Bachelor of Science in Fisheries</h2>
                             <p>Chittagong Veterinary and Animal Science University</p>
--- src/Pages/Banner/Banner.jsx
+++ src/Pages/Banner/Banner.jsx
@@ -20,10 +20,10 @@ const Banner = () => {
                 </h2>
                 <p className="text-lg text-gray-600 mb-7">A passionate frontend developer with expertise in creating sleek, modern, and user-friendly web interfaces.</p>
                 <div className="flex items-center text-[#425d33] gap-5 text-2xl">
-                    <a href="https://github.com/TurjoSiam"><FaGithub /></a>
-                    <a href="https://www.linkedin.com/in/siam-mohammed-abdulah-0193b4162"><FaLinkedin /></a>
-                    <a href="https://x.com/SiamTurjo"><FaTwitter /></a>
-                    <a href="https://www.facebook.com/tur.jo.9275"><FaFacebook /></a>
+                    <a className="hover:text-[#6b8e59]" href="https://github.com/TurjoSiam"><FaGithub /></a>
+                    <a className="hover:text-[#6b8e59]" href="https://www.linkedin.com/in/siam-mohammed-abdulah-0193b4162"><FaLinkedin /></a>
+                    <a className="hover:text-[#6b8e59]" href="https://x.com/SiamTurjo"><FaTwitter /></a>
+                    <a className="hover:text-[#6b8e59]" href="https://www.facebook.com/tur.jo.9275"><FaFacebook /></a>
                 </div>
                 <a target="_blank" href="https://drive.google.com/file/d/1yuazgobYn5TjSt_JKCpBQQhJ3mWpKigB/view?usp=drive_link" className="btn-main flex items-center gap-2"><MdOutlineFileDownload /> Download Resume</a>
             </div>
