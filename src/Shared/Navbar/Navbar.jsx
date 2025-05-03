78f94df90fb8b8f1030f43f3904f4cc8cff69a45
TurjoSiam <t.siam24@gmail.com>
2025-05-03 22:47:53 +0600

    [MODIFIED]-navbar margin and position modified

--- src/Shared/Navbar/Navbar.jsx
+++ src/Shared/Navbar/Navbar.jsx
@@ -17,40 +17,38 @@ const Navbar = () => {
 
 
     return (
-        <div>
-            <div className="navbar bg-[#D2D3C3] border-b backdrop:blur-2xl fixed top-0 z-10 lg:px-20 mx-auto">
-                <div className="navbar-start">
-                    <div className="dropdown">
-                        <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
-                            <svg
-                                xmlns="http://www.w3.org/2000/svg"
-                                className="h-5 w-5"
-                                fill="none"
-                                viewBox="0 0 24 24"
-                                stroke="currentColor">
-                                <path
-                                    strokeLinecap="round"
-                                    strokeLinejoin="round"
-                                    strokeWidth="2"
-                                    d="M4 6h16M4 12h8m-8 6h16" />
-                            </svg>
-                        </div>
-                        <ul
-                            tabIndex={0}
-                            className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow">
-                            {links}
-                        </ul>
+        <div className="navbar bg-[#D2D3C3] border-b backdrop:blur-2xl fixed top-0 z-10 lg:px-20 mx-auto">
+            <div className="navbar-start">
+                <div className="dropdown">
+                    <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            className="h-5 w-5"
+                            fill="none"
+                            viewBox="0 0 24 24"
+                            stroke="currentColor">
+                            <path
+                                strokeLinecap="round"
+                                strokeLinejoin="round"
+                                strokeWidth="2"
+                                d="M4 6h16M4 12h8m-8 6h16" />
+                        </svg>
                     </div>
-                    <a href="/" className="btn btn-ghost text-xl"><span className="text-3xl font-carter hidden md:block font-bold text-[#51733F]">SIAM</span></a>
-                </div>
-                <div className="navbar-center hidden lg:flex">
-                    <ul className="menu menu-horizontal text-sm font-bold px-1">
+                    <ul
+                        tabIndex={0}
+                        className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow">
                         {links}
                     </ul>
                 </div>
-                <div className="navbar-end">
+                <a href="/" className="btn btn-ghost text-xl"><span className="text-3xl font-carter hidden md:block font-bold text-[#51733F]">SIAM</span></a>
+            </div>
+            <div className="navbar-center hidden lg:flex">
+                <ul className="menu menu-horizontal text-sm font-bold px-1">
+                    {links}
+                </ul>
+            </div>
+            <div className="navbar-end">
                 <a target="_blank" href="https://drive.google.com/file/d/1yuazgobYn5TjSt_JKCpBQQhJ3mWpKigB/view?usp=drive_link" className="btn-main flex items-center gap-2"><MdOutlineFileDownload /> Download Resume</a>
-                </div>
             </div>
         </div>
     );
