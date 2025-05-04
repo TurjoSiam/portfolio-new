0a397ece3fd17c46ff28d2d8fa4c28926883e5ef
TurjoSiam <t.siam24@gmail.com>
2025-05-04 11:57:34 +0600

    [ADDED]-craving project details added

--- src/Pages/Craving/Craving.jsx
+++ src/Pages/Craving/Craving.jsx
@@ -1,4 +1,4 @@
-import project3 from "../../assets/project3.jpg"
+import project4 from "../../assets/project4.jpg"
 
 const Craving = () => {
     return (
@@ -7,29 +7,36 @@ const Craving = () => {
             <div className="bg-gradient-to-tr from-sky-100 to-orange-100 w-full mx-auto p-10 rounded-2xl flex flex-col md:flex-row gap-5 pt-32">
 
                 <div>
-                    <img className="w-[1000px] rounded-xl hover:scale-105 transition-all duration-300 ease-in mb-3" src={project3} alt="image" />
+                    <img className="w-[1000px] rounded-xl hover:scale-105 transition-all duration-300 ease-in mb-3" src={project5} alt="image" />
                 </div>
                 <div className="col-span-1 px-7 font-bold space-y-5">
-                    <h2 className="text-3xl lg:text-3xl font-bold">Ripple - A Social Media Forum</h2>
+                    <h2 className="text-3xl lg:text-3xl font-bold">Craving – Food Ordering Website</h2>
                     <h1 className="text-xl underline font-bold md:mb-5">Project Details</h1>
-                    <p>Overview: <span className="font-normal">Ripple is a social media platform where users can post, read, and share their thoughts on various topics. The platform aims to foster knowledge exchange, creativity, and healthy debates, creating a space for community engagement.</span></p>
+                    <p>Overview: <span className="font-normal">Craving is a full-stack food ordering platform featuring cart management, ordering and payment systems and role-based functionality. Advanced filtering and sorting system with location-based restaurant suggestion. Real time order management and introducing donation campaigns.</span></p>
                     <div className="flex items-center flex-wrap gap-3">
-                        <span className="bg-secondary rounded-full font-bold px-3 py-1 text-sm">react</span>
+                        <span className="bg-secondary rounded-full font-bold px-3 py-1 text-sm">next.js</span>
+                        <span className="bg-secondary rounded-full font-bold px-3 py-1 text-sm">typescript</span>
                         <span className="bg-secondary rounded-full font-bold px-3 py-1 text-sm">node.js</span>
                         <span className="bg-secondary rounded-full font-bold px-3 py-1 text-sm">express.js</span>
                         <span className="bg-secondary rounded-full font-bold px-3 py-1 text-sm">mongoDB</span>
-                        <span className="bg-secondary rounded-full font-bold px-3 py-1 text-sm">tailwind CSS</span>
+                        <span className="bg-secondary rounded-full font-bold px-3 py-1 text-sm">shadcn</span>
+                        <span className="bg-secondary rounded-full font-bold px-3 py-1 text-sm">next auth</span>
                     </div>
                     <h2>Challenges faced: <span className="font-normal"><ul className="list-disc ml-10">
-                        <li>Some api was not working due to same route</li>
-                        <li>Problem faced during vercel deploy</li></ul></span></h2>
+                        <li>Difficulties faced exploring new technologies like next.js, typescript.</li>
+                        <li>Had to solve numerous errors before deploying in vercel.</li>
+                        <li>Solved a lot of conflicts in github collaborative repository.</li>
+                    </ul>
+                    </span>
+                    </h2>
                     <h2>Future plans: <span className="font-normal"><ul className="list-disc ml-10">
-                        <li>Role based authentication</li>
-                        <li>Dark mode</li>
+                        <li>Real-time rider's location tracking in map.</li>
+                        <li>Live texting and calling feature with rider and admin.</li>
+                        <li>Weather based food suggestions.</li>
                     </ul></span></h2>
                     <div className="flex items-center gap-5">
-                        <a className=" text-blue-500 underline" href="https://ripple-turjosiam.netlify.app/">Live Demo</a>
-                        <a className=" text-blue-500 underline" href="https://github.com/TurjoSiam/ripple-forum">GitHub Repository</a>
+                        <a className=" text-blue-500 underline" href="craving-pied.vercel.app">Live Demo</a>
+                        <a className=" text-blue-500 underline" href="https://github.com/Shahriarkawsik/Craving">GitHub Repository</a>
                     </div>
                 </div>
             </div>
